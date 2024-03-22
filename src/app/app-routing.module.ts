ec659c27da6a1d6d62ae20f7cfa15b8558422936
Maik Sonnenberg <maik.sonnenberg@hotmail.de>
2024-03-22 18:55:57 +0100

    email senden mit attachments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   {path :'vorlagen', component: VorlagenComponent},
   {path :'emailadressen', component: EmailadressenComponent},
   { path:'senden', component: EmailsendenComponent}
+
 ];
 
 @NgModule({
--- src/app/emailsenden/emailsenden.component.ts
+++ src/app/emailsenden/emailsenden.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./emailsenden.component.scss']
 })
 export class EmailsendenComponent {
+  attachments: File | undefined;
+
+  openFileExplorer(event: any) {
+    const fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      this.attachments = fileList[0];
+      console.log('Selected file:', this.attachments);
+
+      // Hier können Sie die Datei verarbeiten, z.B. hochladen oder weitere Aktionen durchführen
+    }
+
+    //inputElement.click();
+  }
 
 }
